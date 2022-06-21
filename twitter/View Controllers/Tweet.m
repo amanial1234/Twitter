e860b36020c5c5e15aa9c0b13f743af18e4209e2
Aman Abraham <amanial@mit.edu>
2022-06-21 15:46:06 -0700

    Major Changes
    started implementing a class for tweet and made edits to the API manager to adjust for the new dictionary

---
+++ twitter/View Controllers/Tweet.m
@@ -0,0 +1,13 @@
+//
+//  Tweet.m
+//  twitter
+//
+//  Created by Aman Abraham on 6/21/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import "Tweet.h"
+
+@implementation Tweet
+
+@end
---
+++ twitter/View Controllers/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  twitter
+//
+//  Created by Aman Abraham on 6/21/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
